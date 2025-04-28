8ed7d6df66f6198cf03a652a87e02587de6a142a
DeonBeka <deonbeka92@gmail.com>
2025-04-28 21:49:29 +0200

    /

--- JS/home.js
+++ JS/home.js
@@ -3,6 +3,35 @@ function scrollToJoin() {
     joinSection.scrollIntoView({ behavior: 'smooth' });
   }
 
+/** Slide Show */
+var slideIndex = 0;
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+      
+    for(i = 0 ; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+    slides[slideIndex -1].style.display = "block";
+  
+}
+showSlides()
+  
+function plusSlides(n){
+    slideIndex += n;
+    showSlides(slideIndex)
+}
+setInterval(plusSlides, 3000, 1);
+
+
+
   function validateForm(event) {
     event.preventDefault();
 
